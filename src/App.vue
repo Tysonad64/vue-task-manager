d060bc0ef41994c97f60a59fdcfdcd279380b17e
Tyson <tysonad3026@outlook.com>
2025-10-02 12:29:39 -0700

    Added first component boiler plate

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const message = ref('Tasks Manager');
+const newTask = ref('');
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>{{ message }}</h1>
+    <form>
+      <label>
+        New Task
+        <input v-model="newTask" name="newTask">
+      </label>
+      <div class="button-container">
+        <button type="submit">Add Task</button>
+      </div>
+    </form>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+main {
+  max-width: 800px;
+  margin: 1rem auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.button-container {
+  display: flex;
+  justify-content: end;
 }
 </style>

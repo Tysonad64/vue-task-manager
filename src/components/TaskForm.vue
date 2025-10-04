af425f825546ea0d9e9e8f9f0e20b24b52e121c5
Tyson <tysonad3026@outlook.com>
2025-10-04 13:04:36 -0700

    added functionality

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+const emit = defineEmits<{
+    addTask: [newTask: string]; // event name and paylo
+}>();
+
 const newTask = ref('');
+const error = ref('');
 
 function formSubmitted() {
-  console.log(newTask.value);
-}
+    if (newTask.value.trim()){
+        emit('addTask', newTask.value.trim());
+        newTask.value = '';
+    } else {
+        error.value = 'Task cannot be empty';
+    }
+    
+};
+    
 </script>
 
 <template>
   <form @submit.prevent="formSubmitted">
       <label>
         New Task
-        <input v-model="newTask" name="newTask">
+        <input
+         v-model="newTask" 
+         name="newTask" 
+         :aria-invalid="!!error || undefined"
+         @input="error = ''"
+         >
+        <small v-if="error" id="invalid-helper">
+            {{error}}
+        </small>
       </label>
       <div class="button-container">
         <button type="submit">Add Task</button>
